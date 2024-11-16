022065b160f08de50a7e9bf4fafd552fe3f0b07b
Xasan Nosirov <xasannosirov094@gmail.com>
2024-11-16 19:06:27 +0500

    16.11.2024 daily

---
+++ 50/solution.go
@@ -0,0 +1,16 @@
+package main
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1.0
+	} else if n < 0 {
+		return 1.0 / myPow(x, -n)
+	}
+
+	tmp := myPow(x, n/2)
+	if n%2 == 0 {
+		return tmp * tmp
+	} else {
+		return x * tmp * tmp
+	}
+}
