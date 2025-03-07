926727270775c8b52dcc4024f203e1c5c4584051
Xasan Nosirov <xasan.nosirov.interview@gmail.com>
2025-03-07 12:45:27 +0500

    with merge sort

---
+++ 215/solution.go
@@ -0,0 +1,38 @@
+package main
+
+func merge(first, second []int) []int {
+	var sorted []int
+	i, j := 0, 0
+
+	for i < len(first) && j < len(second) {
+		if first[i] < second[j] {
+			sorted = append(sorted, first[i])
+			i++
+		} else {
+			sorted = append(sorted, second[j])
+			j++
+		}
+	}
+
+	sorted = append(sorted, first[i:]...)
+	sorted = append(sorted, second[j:]...)
+
+	return sorted
+}
+
+func mergeSort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+
+	mid := len(nums) / 2
+	first := mergeSort(nums[:mid])
+	second := mergeSort(nums[mid:])
+
+	return merge(first, second)
+}
+
+func findKthLargest(nums []int, k int) int {
+	nums = mergeSort(nums)
+	return nums[len(nums)-k]
+}
