b771104798626b381209f1fcaeb79b3155075aeb
Xasan Nosirov <xasan.nosirov.interview@gmail.com>
2025-12-08 12:33:29 +0500

    08.12.2025 daily

---
+++ 1925/solution.go
@@ -0,0 +1,17 @@
+package solution
+
+func countTriples(n int) int {
+	count := 0
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			for k := 1; k <= n; k++ {
+				if i*i+j*j == k*k {
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
